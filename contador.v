ddf963f86a909ef1a01563c29afcb55eef56a0fe
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-04-17 18:04:19 -0300

    Finalizado

--- contador.v
+++ contador.v
@@ -8,6 +8,16 @@ module Contador (
     output reg [7:0] saida
 );
 
-// Insira seu código aqui
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            saida <= 8'd106;
+        end else begin
+            case ({acrescer, decrecer})
+                2'b01: saida <= saida - 1; 
+                2'b10: saida <= saida + 1;
+                default: saida <= saida; 
+            endcase
+        end
+    end
 
 endmodule
